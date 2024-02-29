bdbac98080981dafe788cc2a50893201c62f0034
janath <janathhma@gmail.com>
2024-02-29 15:20:34 +0530

    create UserController

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+
+    public String helloUserController(){
+        return "User access level";
+    }
+
+}
