e21406412a5dd78accbfabe3a534bbdf79b7dc0f
janath <janathhma@gmail.com>
2024-03-01 22:37:33 +0530

    Create login user method in authenticationService

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,11 @@ c
 3src/main/java/org/example/service/TokenService.java,3\8\38845cadb3e5de2867815cb38b579bc56e800959
 f
 6src/main/java/org/example/models/LoginResponseDto.java,7\4\74419a7de9845243e70f48fd9dd0d310e3f823cb
+o
+?src/main/java/org/example/AuthenticationBackendApplication.java,3\f\3f933f00fb8355b5e1ab74b207bd4cde2348d860
+r
+Bsrc/main/java/org/example/configuration/SecurityConfiguration.java,1\a\1a204fcf5a2ac32af1a10b5638015cbee45a759c
+l
+<src/main/java/org/example/service/AuthenticationService.java,e\f\ef5b46ffefb199fe0afc7cb3a65d2c7eb7bf8936
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/main/java/org/example/service/AuthenticationService.java
+++ src/main/java/org/example/service/AuthenticationService.java
@@ -1,38 +1,68 @@
 package org.example.service;
 
-import jakarta.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.example.Repository.RoleRepository;
 import org.example.Repository.UserRepository;
 import org.example.models.ApplicationUser;
+import org.example.models.LoginResponseDto;
 import org.example.models.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.Set;
 
 @Service
 @Transactional
 public class AuthenticationService {
+
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private RoleRepository roleRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public ApplicationUser registerUser(String username,String password){
-        String encodePassword = passwordEncoder.encode(password);
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public ApplicationUser registerUser(String username, String password){
+
+        String encodedPassword = passwordEncoder.encode(password);
         Role userRole = roleRepository.findByAuthority("USER").get();
 
-        Set<Role> authority = new HashSet<>();
-        authority.add(userRole);
+        Set<Role> authorities = new HashSet<>();
 
-        return userRepository.save(new ApplicationUser(0,username,encodePassword,authority));
+        authorities.add(userRole);
+
+        return userRepository.save(new ApplicationUser(0, username, encodedPassword, authorities));
     }
 
+    public LoginResponseDto loginUser(String username, String password){
 
+        try{
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
 
+            String token = tokenService.generateJwt(auth);
+
+            return new LoginResponseDto(userRepository.findByUsername(username).get(), token);
+
+        } catch(AuthenticationException e){
+            return new LoginResponseDto(null, "");
+        }
+    }
 
 }
