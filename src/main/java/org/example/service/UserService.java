64fd9c0621709e7552a9fbab02cea83631c78100
janath <janathhma@gmail.com>
2024-03-01 06:46:23 +0530

    service layer connect to repository layer to get users details

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,6 +1,5 @@
 package org.example.service;
-import org.example.models.ApplicationUser;
-import org.example.models.Role;
+import org.example.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,23 +7,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder encoder;
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         System.out.println("In the user details service");
-        if(!username.equals("Ethen")) throw new UsernameNotFoundException("Not Ether");
-
-        Set<Role> role = new HashSet<>();
-        role.add(new Role(1,"USER"));
-
-        return new ApplicationUser(1,"Ethan",encoder.encode("password"),role);
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User is not valid"));
 
     }
 }
